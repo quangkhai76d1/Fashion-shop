e107687ab432514f46e136c233b91e733768f47e
quangkhai76d1 <18520876@gm.uit.edu.vn>
2022-06-04 23:03:53 +0700

    make hero-slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Helmet from "../components/Helmet";
+import HeroSlider from "../components/HeroSlider";
+import heroSliderData from "../assets/fake-data/hero-slider";
 
 const Home = () => {
-  return <Helmet title='Trang chủ'>Home</Helmet>;
+  return (
+    <Helmet title='Trang chủ'>
+      <HeroSlider
+        data={heroSliderData}
+        control={true}
+        auto={true}
+        timeOut={5000}
+      />
+    </Helmet>
+  );
 };
 
 export default Home;
