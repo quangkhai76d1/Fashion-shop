0292f8b22da7e598df263cabd6c8b576f808ba9d
quangkhai76d1 <18520876@gm.uit.edu.vn>
2022-06-10 12:46:15 +0700

    make product view  page

---
+++ src/components/ProductView.jsx
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { withRouter } from "react-router";
+
+const ProductView = (props) => {
+  const product = props.product;
+
+  const [imageView, setImageView] = useState(product.image01);
+
+  const [showContent, setShowContent] = useState(false);
+
+  const [color, setColor] = useState(undefined);
+  const [size, setSize] = useState(undefined);
+
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    setImageView(product.image01);
+    setShowContent(undefined);
+    setQuantity(1);
+    setSize(undefined);
+  }, [product]);
+
+  const check = () => {
+    if (color === undefined) {
+      alert("Vui lòng chọn màu sắc");
+      return false;
+    }
+
+    if (size === undefined) {
+      alert("Vui lòng chọn kích cỡ");
+      return false;
+    }
+    return true;
+  };
+
+  const addToCart = () => {
+    if (check()) console.log({ color, size, quantity });
+  };
+
+  const goToCart = () => {
+    if (check()) props.history.push("/cart");
+  };
+  return (
+    <div className='product'>
+      <div className='product__image'>
+        <div className='product__image__list'>
+          <div
+            className='product__image__list__item'
+            onClick={() => setImageView(product.image01)}
+          >
+            <img src={product.image01} alt='' />
+          </div>
+          <div
+            className='product__image__list__item'
+            onClick={() => setImageView(product.image02)}
+          >
+            <img src={product.image02} alt='' />
+          </div>
+        </div>
+        <div className='product__image__main'>
+          <img src={imageView} alt='' />
+        </div>
+
+        <div className={`product-desc ${showContent ? "expand" : ""}`}>
+          <div className='product-desc__title'>Chi tiết sản phẩm</div>
+          <div
+            className='product-desc__content'
+            dangerouslySetInnerHTML={{ __html: product.description }}
+          ></div>
+          <div className='product-desc__toggle'>
+            <Button size='sm' onClick={() => setShowContent(!showContent)}>
+              {!showContent ? "Xem thêm" : "Thu gọn"}
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className='product__info'>
+        <h1 className='product__info__title'>{product.title}</h1>
+
+        <div className='product__info__item'>
+          <span className='product__info__item__price'>
+            {Number(product.price)}
+          </span>
+        </div>
+
+        <div className='product__info__item'>
+          <div className='product__info__item__title'>Màu sắc</div>
+          <div className='product__info__item__list'>
+            {product.colors.map((item, index) => (
+              <div
+                key={index}
+                className={`product__info__item__list__item ${
+                  color === item ? "active" : ""
+                }`}
+                onClick={() => setColor(item)}
+              >
+                <div className={`circle bg-${item}`}></div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className='product__info__item'>
+          <div className='product__info__item__title'>Kích cỡ</div>
+          <div className='product__info__item__list'>
+            {product.size.map((item, index) => (
+              <div
+                key={index}
+                className={`product__info__item__list__item ${
+                  size === item ? "active" : ""
+                }`}
+                onClick={() => setSize(item)}
+              >
+                <span className='product__info__item__list__item__size'>
+                  {item}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className='product__info__item'>
+          <div className='product__info__item__title'>Số lượng</div>
+          <div className='product__info__item__quantity'>
+            <div
+              className='product__info__item__quantity__btn'
+              onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
+            >
+              <i className='bx bx-minus'></i>
+            </div>
+            <div className='product__info__item__quantity__input'>
+              {quantity}
+            </div>
+            <div
+              className='product__info__item__quantity__btn'
+              onClick={() => setQuantity(quantity + 1)}
+            >
+              <i className='bx bx-plus'></i>
+            </div>
+          </div>
+        </div>
+
+        <div className='product__info__item'>
+          <Button onClick={addToCart}>Thêm vào giỏ hàng</Button>
+          <Button onClick={goToCart}>Mua ngay</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductView.propTypes = {
+  product: PropTypes.object.isRequired,
+};
+
+export default withRouter(ProductView);
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Helmet from "../components/Helmet";
+import productData from "../assets/fake-data/products";
+import Section, { SectionBody, SectionTitle } from "../components/Section";
+import Grid from "../components/Grid";
+import ProductCard from "../components/ProductCard";
+import ProductView from "../components/ProductView";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = (props) => {
+  const product = productData.getProductBySlug(props.match.params.slug);
+
+  return (
+    <Helmet title={product.title}>
+      <Section>
+        <SectionBody>
+          <ProductView product={product} />
+        </SectionBody>
+      </Section>
+
+      <Section>
+        <SectionTitle>Khám phá thêm</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(8).map((item, index) => (
+              <ProductCard
+                key={index}
+                name={item.title}
+                img01={item.image01}
+                img02={item.image02}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
+    </Helmet>
+  );
 };
 
 export default Product;
